78265160abf040f0b321234c086c012e6c5cb3a1
matias <galarzamatias352@gmail.com>
2024-02-03 11:34:13 -0300

    Cambios menores:30min +Se alinearon los botones con la seccion de display -Avanzar con la parte de historial, ejecutar la api para realizar operaciones

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,16 +1,14 @@
-<div class="items-center">
-    <h1 class="text-center text-3xl bg-gray-300 font-bold pb-1 mb-2">Calculadora Básica</h1>
-    <div class="bg-black py-7 text-white m-2">
-        <p></p>
-    </div>
-    
+<h1 class="text-center text-3xl bg-gray-300 font-bold pb-1 mb-2">Calculadora Básica</h1>
+<div class="bg-black py-7 text-white m-2">
+    <p></p>
+</div>
+<div class="m-2">
     <div class="flex w-full py-1 gap-1">
         <button class="btn btn-orange flex-1">CE</button>
         <button class="btn btn-orange flex-1">C</button>
         <button class="btn btn-red flex-1">±</button>
         <button class="btn btn-red flex-1">%</button>
     </div>
-    
     <div class="flex w-full py-1 gap-1">
         <button class="btn btn-blue flex-1">7</button>
         <button class="btn btn-blue flex-1">8</button>
@@ -40,7 +38,7 @@
 </div>
 <style>
    .btn{
-    @apply flex-grow text-center p-4;
+    @apply flex-grow text-center p-4; /* Asegura que cada botón se expanda para llenar el espacio asignado */
    } 
 .btn-blue{
     @apply bg-blue-700 text-white;
