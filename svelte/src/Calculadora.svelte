d254f1e6d6659ef6685795b61b0cafd037df3ed0
matias <galarzamatias352@gmail.com>
2024-02-04 11:54:16 -0300

    Mejoras en Front-end:3hs +Implentar la modificacion del display,cambios en el diselo del historial y navegacion entre las paginas -Terminar la seccion de calculadora y implementar las operaciones

---
+++ svelte/src/Calculadora.svelte
@@ -0,0 +1,63 @@
+<script>
+    let display='';
+    let clear_display= ()=>display='';
+    let num1,num2;
+    let op;
+    function addDisplay(value) {
+      display+=value;  
+    }
+</script>
+<div class="bg-black py-7 text-white m-2">
+    <p>{display}</p>
+</div>
+<div class="m-2">
+    <div class="flex w-full py-1 gap-1">
+        <button class="btn btn-orange" on:click={() => clear_display()}>CE</button>
+        <button class="btn btn-orange">C</button>
+        <button class="btn btn-red">±</button>
+        <button class="btn btn-red" >%</button>
+    </div>
+    <div class="flex w-full py-1 gap-1">
+        <button class="btn btn-blue"on:click={() => addDisplay(7)}>7</button>
+        <button class="btn btn-blue"on:click={() => addDisplay(8)}>8</button>
+        <button class="btn btn-blue"on:click={() => addDisplay(9)}>9</button>
+        <button class="btn btn-red" on:click={() => addDisplay("÷")}>÷</button>
+    </div>
+    
+    <div class="flex w-full py-1 gap-1">
+        <button class="btn btn-blue"on:click={() => addDisplay(4)}>4</button>
+        <button class="btn btn-blue"on:click={() => addDisplay(5)}>5</button>
+        <button class="btn btn-blue"on:click={() => addDisplay(6)}>6</button>
+        <button class="btn btn-red" on:click={() => addDisplay("x")}>x</button>
+    </div>
+    
+    <div class="flex w-full py-1 gap-1">
+        <button class="btn btn-blue"on:click={() => addDisplay(1)}>1</button>
+        <button class="btn btn-blue"on:click={() => addDisplay(2)}>2</button>
+        <button class="btn btn-blue"on:click={() => addDisplay(3)}>3</button>
+        <button class="btn btn-red" on:click={() => addDisplay("-")}>-</button>
+    </div>
+    <div class="flex w-full py-1 gap-1">
+        <button class="btn btn-blue"on:click={() => addDisplay(0)}>0</button>
+        <button class="btn btn-blue" on:click={() => addDisplay(".")}>.</button>
+        <button class="btn btn-green">=</button>
+        <button class="btn btn-red" on:click={() => addDisplay("+")}>+</button>
+    </div>
+</div>
+<style>
+   .btn{
+    @apply flex-grow text-center p-4 flex-1; /* Asegura que cada botón se expanda para llenar el espacio asignado */
+   } 
+.btn-blue{
+    @apply bg-blue-700 text-white;
+}
+.btn-orange{
+    @apply bg-orange-400 text-white;
+}
+.btn-green{
+    @apply bg-green-600 text-white;
+}
+.btn-red{
+    @apply bg-red-800 text-white;
+}
+</style>
--- svelte/src/historial.svelte
+++ svelte/src/historial.svelte
@@ -10,25 +10,40 @@
      }).catch(error => {
             console.error("Error al obtener el historial:", error);
         });
-     
     }
-    onMount(getHistorial);
-    
-    </script>
-    {#await historial}
+    onMount(getHistorial);    
+</script>
+<h1 class="text-center font-bold text-3xl">Historial</h1>
+<div class="min-h-screen m-4 bg-black p-4"> <!-- Contenedor principal con margen, color de fondo y padding -->
+    <table class="w-full text-white"> <!-- Tabla de ancho completo con texto blanco -->
+        <thead>
+            <tr>
+                <th class="table-cell">Fecha</th>
+                <th class="table-cell">Operacion</th>
+                <th class="table-cell">Resultado</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#await historial}
         <p>Cargando...</p>
     {:then}
-        {#if historial}
-            {#if historial.length === 0}
-                <p>Todavia no hay operaciones realizadas</p>
-            {:else}
-                {#each historial as item}
-                <p>{item.fecha} {item.operacion} {item.resultado}  </p>
-                {/each}
-            {/if}
-        {:else}
-            <p>No hay datos disponibles.</p>
-        {/if}
+        {#if historial && historial.length != 0}
+            {#each historial as item}
+            <tr>
+                <td class="table-cell">{item.fecha}</td>
+                <td class="table-cell">{item.operacion}</td>
+                <td class="table-cell">{item.resultado}</td>
+            </tr>
+            {/each}
+        {/if}        
     {:catch error}
         <p>Error: {error}</p>
     {/await}
+        </tbody>
+    </table>
+</div>
+<style>
+   .table-cell{
+    @apply px-4 py-2 text-center;
+   }
+</style>
